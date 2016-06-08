1e9903603eb092edf12110b8ff02c2ba8d16592e
fanzhengchen <fanzhengchen@gmail.com>
2016-06-08 14:55:04 +0800

    init

---
+++ app/src/main/java/mintcode/com/workhub_im/LoginActivity.java
@@ -0,0 +1,12 @@
+package mintcode.com.workhub_im;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mark on 16-6-8.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+
+
+}
